712b3cf365f587daeddffc0eca87b81da2178262
Random Xyzzy <rndmxyzzy@gmail.com>
2019-03-15 01:00:25 +0900

    change distribution url

--- hack-details.meta.js
+++ hack-details.meta.js
@@ -2,18 +2,14 @@
 // @id             iitc-plugin-hack-details@randomizax
 // @name           IITC plugin: Portal Hack Details
 // @category       Layer
-// @version        1.0.0.20181124.13837
+// @version        1.0.1.20190314.160025
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://rawgit.com/randomizax/hack-details/latest/hack-details.meta.js
-// @downloadURL    https://rawgit.com/randomizax/hack-details/latest/hack-details.user.js
-// @description    [randomizax-2018-11-24-013837] Show portal hack details on map.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @include        https://intel.ingress.com/intel*
-// @include        http://intel.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @match          https://intel.ingress.com/intel*
-// @match          http://intel.ingress.com/intel*
+// @updateURL      https://randomizax.github.io/hack-details/hack-details.meta.js
+// @downloadURL    https://randomizax.github.io/hack-details/hack-details.user.js
+// @description    [randomizax-2019-03-14-160025] Show portal hack details on map.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
